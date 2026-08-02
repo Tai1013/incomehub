f3df6615ea8d160a2d01dd68f449ae1f4c7c893a
Tai1013 <frank22199215@gmail.com>
2026-08-03 01:52:01 +0800

    fix: 移除未使用的 computed 引入

--- src/components/auth/ChangePasswordDialog.vue
+++ src/components/auth/ChangePasswordDialog.vue
@@ -51,7 +51,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, reactive, computed } from 'vue'
+import { ref, reactive } from 'vue'
 import type { FormInstance, FormRules } from 'element-plus'
 import { ElMessage } from 'element-plus'
 import { useAuthStore } from '../../stores/auth'
